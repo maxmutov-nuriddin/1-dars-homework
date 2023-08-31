15a4b1f20d706b97dae4d6b6bb55ee7a64e1a951
maxmutov-nuriddin <mahmudovnuriddin35@gmail.com>
2023-08-31 16:55:46 +0500

    add main

--- maket/src/App.jsx
+++ maket/src/App.jsx
@@ -12,12 +12,14 @@ function App() {
   return (
     <>
       <Header />
-      <Hero />
-      <About />
-      <Setvices />
-      <Follow />
-      <Articles />
-      <Register />
+      <main>
+        <Hero />
+        <About />
+        <Setvices />
+        <Follow />
+        <Articles />
+        <Register />
+      </main>
       <Footer />
     </>
   )
